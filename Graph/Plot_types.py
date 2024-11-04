a3061bbd8c0332c073ad6f1b386b2c36e056408c
Craig-Venables <64995521+Craig-Venables@users.noreply.github.com>
2024-11-04 18:43:40 +0000

    added in dc endurance + save

--- Graph/Plot_types.py
+++ Graph/Plot_types.py
@@ -44,7 +44,7 @@ def polar_subplot(x, y):
     fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection='polar'))
     ax1.plot(x, y)
     ax2.plot(x, y ** 2)
-
+#
 
 def sclc_ps(voltage, current_density, fontsize=8):
     # create scatter
